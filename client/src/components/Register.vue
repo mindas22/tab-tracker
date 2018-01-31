03986688fb2e6b9feda6c7c075f9b8b6cd6f0f48
Mindas <mindaugas.lickus@webelevate.ie>
2018-01-31 01:12:43 +0000

    added sqlite and vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
+  <v-layout column>
+  <v-flex xs6 offset-xs3>
+    <div class="white elevation-2">
+      <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar-title>Register</v-toolbar-title>
+      </v-toolbar>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+      <input
     type="email"
     name="email"
     v-model="email"
@@ -13,11 +18,16 @@
     v-model="password"
     placeholder="password" />
     <br>
-    <button
+    <div class="error" v-html="error" />
+    <v-btn
+      class="cyan"
       @click="register">
       Register
-    </button>
-  </div>
+    </v-btn>
+      </div>
+    </div>
+  </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -26,20 +36,28 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.error {
+  color: red;
+}
 
 </style>

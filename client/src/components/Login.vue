593d6d73d408c3c6a2462e97195f57dea1bc354a
Mindas <mindaugas.lickus@webelevate.ie>
2018-02-03 00:08:27 +0000

    Stage 3. Added login page, bcrypt, vuex, store, header

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,33 +3,25 @@
   <v-flex xs6 offset-xs3>
     <div class="white elevation-2">
       <v-toolbar flat dense class="cyan" dark>
-        <v-toolbar-title>Register</v-toolbar-title>
+        <v-toolbar-title>Login</v-toolbar-title>
       </v-toolbar>
       <div class="pl-4 pr-4 pt-2 pb-2">
-        <form
-          name="register-form"
-          autocomplete="off">
-          <v-text-field
-            label="Email"
-            v-model="email">
-          </v-text-field>
-        </form>
+      <v-text-field
+        label="Email"
+        v-model="email">
+      </v-text-field>
     <br>
       <v-text-field
         label="Password"
         v-model="password"
-        autocomplete="new-password"
-        min="8"
-        hint="At least 8 characters, max 32."
-        type="password"
-        counter>
+        type="password">
       </v-text-field>
     <br>
     <div class="error" v-html="error"/>
     <v-btn
       class="cyan" dark
-      @click="register">
-      Register
+      @click="login">
+      Login
     </v-btn>
       </div>
     </div>
@@ -48,9 +40,9 @@ export default {
     }
   },
   methods: {
-    async register () {
+    async login () {
       try {
-        const response = await AuthenticationService.register({
+        const response = await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
